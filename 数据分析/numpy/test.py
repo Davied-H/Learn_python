c08688756ef0ce9edf53eb0607c5a8b06da5dc89
Dong Dong <mz@email.com>
2019-03-03 14:27:23 +0800

    git push

--- 数据分析/numpy/test.py
+++ 数据分析/numpy/test.py
@@ -1,8 +1,14 @@
 import  numpy as np
 
 #将列表转换为矩阵
-array = np.array([[1,2,3],[2,3,4],[3,4,5]])
-print("array is :",array)
-print("number of dim:",array.ndim) # 维度
-print("shape:",array.shape) # 行数 和 列数
-print("size:",array.size) # 元素个数
+# array = np.array([[1,2,3],[2,3,4],[3,4,5]])
+# print("array is :",array)
+# print("number of dim:",array.ndim) # 维度
+# print("shape:",array.shape) # 行数 和 列数
+# print("size:",array.size) # 元素个数
+
+#设置元素类型
+# array = np.array([1,2,3],dtype=np.int64) #默认是int = int32
+# print(array.dtype)
+# array = np.array([1,2,3],dtype=np.float(1)) #默认为float64 , 位数越大占用空间越大
+# print(array)
