a8ca236ab895217982df1398ed64757e0257a195
Dong Dong <mz@email.com>
2019-03-03 13:54:40 +0800

    git push

---
+++ 数据分析/numpy/test.py
@@ -0,0 +1,8 @@
+import  numpy as np
+
+#将列表转换为矩阵
+array = np.array([[1,2,3],[2,3,4],[3,4,5]])
+print("array is :",array)
+print("number of dim:",array.ndim) # 维度
+print("shape:",array.shape) # 行数 和 列数
+print("size:",array.size) # 元素个数
