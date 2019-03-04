60b4d0112f25dfd94fbc0dfafad604df79ee8b4c
hedongdong <13331190665@163.com>
2019-03-04 12:59:26 +0800

    git push

--- 爬虫/learning/爬取公司班级.py
+++ 爬虫/learning/爬取公司班级.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-base_url = "http://www.topeduol.com"
-html = urlopen(base_url).read().decode("utf-8")
-soup = BeautifulSoup(html,"lxml")
+base_url = "http://www.topeduol.com" #url
+html = urlopen(base_url).read().decode("utf-8") #使用urlopen函数访问网页并获取html页面
+soup = BeautifulSoup(html,"lxml") #使用bs4实例化
 all_span = soup.find_all("span",{"class":re.compile(r"atit$")})
 num = 0
 for i in all_span:
@@ -14,11 +14,14 @@ print(num)
 
 print("-------------------------------------------")
 
-all_teacher = soup.find_all("div",{"class":"a2"})
+all_teacher = soup.find_all("div",{"class":"pic",})
 for teacher in all_teacher :
-    print(teacher.string)
-
-
+    div_list = teacher.contents #将span标签所以子节点存入list中
+    for div in range(len(div_list)-1,-1,-1): #使用range反序循环，正序容易出现下标越界错误
+        if div_list[div] == '\n':
+            div_list.remove('\n')
+    print(div_list[1].string)
+    # print(teacher.contents)
 print("-------------------------------------------")
 
 all_message = soup.find_all("div",{"class":"link-item-t"})
